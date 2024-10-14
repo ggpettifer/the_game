ca6a6b7c828bee5f674890272b1c8b27fb1ca2a4
ggpettifer <ggpettifer@gmail.com>
2024-10-14 22:18:57 +0100

    render current player box green

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import eventlet
 eventlet.monkey_patch()  # Patching required for eventlet to work properly with asyncio
 
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 from threading import Lock
 
@@ -40,14 +39,14 @@ def timer_worker():
             game.increment_current_player_time()
 
             if game.player1_time >= game.limit:
-                socketio.emit('winner_update', {'winner': 'Player 2'})
+                socketio.emit('winner_update', {'winner': '2'})
                 game.is_active = False
             elif game.player2_time >= game.limit:
-                socketio.emit('winner_update', {'winner': 'Player 1'})
+                socketio.emit('winner_update', {'winner': '1'})
                 game.is_active = False
 
             # Broadcast the times to all connected users
-            socketio.emit('timer_update', {'player1_time': game.player1_time, 'player2_time': game.player2_time})
+            socketio.emit('timer_update', {'player1_time': game.player1_time, 'player2_time': game.player2_time, 'current_player': game.current_player})
 
 
 @app.route('/')
@@ -75,7 +74,7 @@ def reset():
         game.player2_time = 0
         game.current_player = 1
         socketio.emit('winner_update', {'winner': ''})
-        socketio.emit('timer_update', {'player1_time': game.player1_time, 'player2_time': game.player2_time})
+        socketio.emit('timer_update', {'player1_time': game.player1_time, 'player2_time': game.player2_time, 'current_player': 0})
     return redirect(url_for('index'))
 
 @app.route('/set_limit', methods=['POST'])
@@ -88,6 +87,6 @@ def set_limit():
 
 if __name__ == '__main__':
     game = Game()
-    lock = Lock()  # Lock to avoid race conditions
+    lock = Lock()  # Lock to avoid race conditions - Both the timer_worker and the app (which handles HTTP requests) need to access game data from the game object concurrently
     socketio.start_background_task(target=timer_worker) # seperate thread
     socketio.run(app, debug=True)
